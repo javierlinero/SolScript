3ca8fef7c6a5c34d41053e19a94ee0347bbf6de6
javierlinero <jl5951@princeton.edu>
2024-04-04 10:46:24 -0400

    setup basic fine-tuning

--- src/run.py
+++ src/run.py
@@ -1,15 +1,46 @@
-#quick layout
+import openai
+import pythx
+import json
 
 def main():
+    # https://platform.openai.com/docs/guides/fine-tuning/use-a-fine-tuned-model
     # load sb-curated
 
     # pre-process data to i/o for openai llm
 
+    # {"prompt": "<prompt text>", "completion": "<ideal generated text>"}
+    # {"prompt": "<prompt text>", "completion": "<ideal generated text>"}
+    # {"prompt": "<prompt text>", "completion": "<ideal generated text>"}
+
+    # jsonify data into "train.json"
+
+    client = openai.OpenAI()
+
+    # create openai file
+    client.files.create(
+        file=open("train.json", "rb"),
+        purpose="fine-tune"
+    )
+
     # setup open ai w/ relative API key in .env
+
+    client.fine_tuning.jobs.create(
+        training_file="train.json", 
+        model="gpt-3.5-turbo"
+    )
 
     # train model w/ curated data 
 
     # create output w/ prompt 
+
+    completion = client.chat.completions.create(
+        model="ft:gpt-3.5-turbo:my-org:custom_suffix:id",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Hello!"}
+        ]
+    )
+    print(completion.choices[0].message)
 
     # feed prompt thru loop to 3 different llm models with specified prompt to score based on number of vulnerabities
 
@@ -17,6 +48,7 @@ def main():
 
     # then we test out some prompts against a vanilla model with mythx interface and see which one performs better
 
+    # https://github.com/Consensys/pythx <- python interface for mythx
 
 
     return None
