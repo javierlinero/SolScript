58bbb57c3b9e251c087e5c36d85e3dfb6874e7e2
javierlinero <jl5951@princeton.edu>
2024-04-04 10:16:40 -0400

    added sb files & setup layout for the entire process

---
+++ src/run.py
@@ -0,0 +1,25 @@
+#quick layout
+
+def main():
+    # load sb-curated
+
+    # pre-process data to i/o for openai llm
+
+    # setup open ai w/ relative API key in .env
+
+    # train model w/ curated data 
+
+    # create output w/ prompt 
+
+    # feed prompt thru loop to 3 different llm models with specified prompt to score based on number of vulnerabities
+
+    # collect scores and feed each one individually to retrain w/ relative score
+
+    # then we test out some prompts against a vanilla model with mythx interface and see which one performs better
+
+
+
+    return None
+
+if __name__ == '__main__':
+    main()
